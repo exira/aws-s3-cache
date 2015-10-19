79dbebbdbfbc6451020bb66693acf1104a371283
Team City <info@exira.com>
2015-10-19 22:32:06 +0000

    Bump version to 0.1.6

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -3,11 +3,11 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("aws-s3-cache")>]
 [<assembly: AssemblyProductAttribute("Exira.AwsS3Cache")>]
-[<assembly: AssemblyDescriptionAttribute("Exira.AwsS3Cache is IIS module which reads cached objects from AWS S3 and quickly serves them to end users")>]
-[<assembly: AssemblyVersionAttribute("0.1.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.5")>]
-[<assembly: AssemblyMetadataAttribute("githash","99b0936ede0097d6838c25cf9853240790ea2f15")>]
+[<assembly: AssemblyDescriptionAttribute("Exira.AwsS3Cache is an IIS module which reads cached objects from AWS S3 and quickly serves them to end users")>]
+[<assembly: AssemblyVersionAttribute("0.1.6")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.6")>]
+[<assembly: AssemblyMetadataAttribute("githash","cf09ec993c11c205a936603c793fafa6bf1a463d")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.5"
+    let [<Literal>] Version = "0.1.6"
